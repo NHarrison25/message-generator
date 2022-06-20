db61e37f378cb3cf500ec2a3a5fab16998a9cb9e
NHarrison25 <NHarrison25@users.noreply.github.com>
2022-06-19 23:32:17 -0400

    Fix Gary Cooper name

--- resources/js/message-generator.js
+++ resources/js/message-generator.js
@@ -1,6 +1,6 @@
 //Quotes broken down into 3 strings
 const part1 = ['It\’s good to be in', 'Those who want', 'They say every day\’s a', 'Well, when you\’re married, you\’ll', 'If you can quote the rules,', 'What happened to', 'A wrong decision', 'Some people are so far behind in a', 'There\'s no', 'C\'est que ce, message generator broken?'];
-const part2 = ['something', 'respect,', 'gift', 'understand', 'then you can', 'Garry Cooper,', 'is better than', 'race', 'chemical solution'];
+const part2 = ['something', 'respect,', 'gift', 'understand', 'then you can', 'Gary Cooper,', 'is better than', 'race', 'chemical solution'];
 const part3 = ['from the ground floor.', 'give respect.', 'but why does it have to be a pair of socks?', 'the importance of fresh produce.', 'obey them.', 'the strong, silent type?', 'indecision.', 'that they actually believe they\'re leading.', 'to a spiritual problem.'];
 
 //Return a random element of a given array
