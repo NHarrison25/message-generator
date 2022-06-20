b58c023bad70ff046b24aa05cb7d0d931870557f
NHarrison25 <NHarrison25@users.noreply.github.com>
2022-06-19 21:58:01 -0400

    Add Artie

--- resources/js/message-generator.js
+++ resources/js/message-generator.js
@@ -1,5 +1,5 @@
 //Quotes broken down into 3 strings
-const part1 = ['It\’s good to be in', 'Those who want', 'They say every day\’s a', 'Well, when you\’re married, you\’ll', 'If you can quote the rules,', 'What happened to', 'A wrong decision', 'Some people are so far behind in a', 'There\'s no'];
+const part1 = ['It\’s good to be in', 'Those who want', 'They say every day\’s a', 'Well, when you\’re married, you\’ll', 'If you can quote the rules,', 'What happened to', 'A wrong decision', 'Some people are so far behind in a', 'There\'s no', 'C\'est que ce, message generator broken?'];
 const part2 = ['something', 'respect,', 'gift', 'understand', 'then you can', 'Garry Cooper,', 'is better than', 'race', 'chemical soultion'];
 const part3 = ['from the ground floor.', 'give respect.', 'but why does it have to be a pair of socks?', 'the importance of fresh produce.', 'obey them.', 'the strong, silent type?', 'indecision.', 'that they actually believe they\'re leading.', 'to a spiritual problem.'];
 
@@ -11,9 +11,16 @@ function pickRandom(array) {
 //Change empty element to contain concatenated string
 function displayMessage(event) {
   let str1 = pickRandom(part1);
-  let str2 = pickRandom(part2);
-  let str3 = pickRandom(part3);
-  //document.getElementById('*idhere*').innerHTML = `${str1} ${str2} ${str3}`;
+  
+  if (str1 === part1[9]) {
+    //display image
+    document.getElementsByTagName('p')[0].innerHTML = `${str1}`;
+  } else {
+    let str2 = pickRandom(part2);
+    let str3 = pickRandom(part3);
+    document.getElementsByTagName('p')[0].innerHTML = `${str1} ${str2} ${str3}`;
+  }
 }
 
-//button.addEventListener('click', displayMessage);
+//Add event handler to button
+getElementsByClass('btn')[0].addEventListener('click', displayMessage);
